1037c679bfc400c0242e03f668dc33649d583a7e
Edmond Su <133119772+deverstater07@users.noreply.github.com>
2023-06-08 18:18:02 +1000

    Finished commenting, changed prompt text wording

--- assets/script.js
+++ assets/script.js
@@ -107,81 +107,82 @@ function writePassword() {
 
   //generate password
   function generatePassword(){
-    
+    // Creating variable passwordLength for later use
     var passwordLength;
+    // Creating array approvedCharacters for later use
     var approvedCharacters = [];
+    // Creating passwordCharacters for later use
     var passwordCharacters = [];
 
     //Function to get password length
     function getLength(){
-      //Ask user for a password length between 8-128 characters
+      //Ask user for a password length between 8-128 characters and assigning that number to variable passwordLength
       passwordLength = window.prompt("How many characters would you like your password to contain?(between 8-128):");
       //Checks to make sure passwordLength is a number between 8-128
+      //Checks if user clicked on cancel for the prompt
       if (passwordLength == null || passwordLength == undefined) {
         window.alert("Password generation has been cancelled.")
-      } else if (passwordLength == ''){
-        window.alert("Nothing was entered, please input a number.")
+        //Checks if anything was input and if it is a number
+      } else if (passwordLength == ''|| isNaN(passwordLength)){
+        window.alert("Please input a number.")
         getLength()
+        //Checks if the number input is less than 8
       } else if (passwordLength < 8) {
         window.alert("Password length must be 8 or more characters.")
         getLength()
+        //Checks if number input is greater than 128
       } else if (passwordLength > 128) {
         window.alert("Password length must be 128 or less characters.")
-        getLength() 
-      } else if (isNaN(passwordLength)){
-        window.alert("Please input a number")
         getLength()
       } else {
-        console.log(passwordLength)
         getCriteria()
-
       }
     }
     
     //Asking for password character criteria
     function getCriteria(){
 
-      //Ask if user would like to include numeric characters
-      var includeNumeric = window.confirm("Would you like to include numeric characters?");
+      //Ask if user would like to include numeric characters and assigning boolean result to variable includeNumeric
+      var includeNumeric = window.confirm("Click 'OK' to include numeric characters?");
       console.log(includeNumeric);
 
-      // If user says yes add numeric characters array to array of approved characters
+      // If user confirmed adding numeric characters, add numeric characters array to array of approved characters
       if (includeNumeric) {
         approvedCharacters = approvedCharacters.concat(numericCharacters);
         console.log(approvedCharacters);
       };
 
-      //Ask if user would like to include lower case letters
-      var includeLower = window.confirm("Would you like to include lower case letters?");
+      //Ask if user would like to include lower case letters and assigning boolean result to variable to includeLower
+      var includeLower = window.confirm("Click 'OK' to include lower case letters?");
       console.log(includeLower);
 
-      // If user says yes add lower case characters array to array of approved characters
+      // If user confirmed adding lower case characters, add lower case characters array to array of approved characters
       if (includeLower){
         approvedCharacters = approvedCharacters.concat(lowerCasedCharacters);
         console.log(approvedCharacters);
       };
 
-      //Ask if user would like to include upper case letters
-      var includeUpper = window.confirm("Would you like to include upper case letters?");
+      //Ask if user would like to include upper case letters and assigning boolean result to includeUpper
+      var includeUpper = window.confirm("Click 'OK' to include upper case letters?");
       console.log(includeUpper);
 
-      // If user says yes add upper case characters array to array of approved characters
+      // If user confirmed adding upper case characters, add upper case characters array to array of approved characters
       if (includeUpper){
         approvedCharacters = approvedCharacters.concat(upperCasedCharacters);
         console.log(approvedCharacters);
       };
 
-      //Ask if user would like to include special characters
-      var includeSpecial = window.confirm("Would you like to include special characters?");
+      //Ask if user would like to include special characters and assigning boolean result to includeSpecial
+      var includeSpecial = window.confirm("Click 'OK' to include special characters?");
       console.log(includeSpecial);
 
-      // If user says yes add special characters array to array of approved characters
+      // If user confirmed adding special characters, add special characters array to array of approved characters
       if (includeSpecial){
         approvedCharacters = approvedCharacters.concat(specialCharacters);
         console.log(approvedCharacters);
       };
       
-      //Checks to make sure user included at least 1 character type
+      //Checks to make sure user confirmed including at least 1 character type
       if (!includeLower && !includeNumeric && !includeSpecial && !includeUpper){
         window.alert("Please include some character types.");
         getCriteria();
